1c281e655cfffc64f9bd92a42b3a0697d38178f4
chenjie180 <1037989610@qq.com>
2020-10-28 11:00:36 +0800

    把登录部分的代码提交到git仓库

--- src/main.js
+++ src/main.js
@@ -3,9 +3,19 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import ElementUI from 'element-ui' //element-ui的全部组件
+import 'element-ui/lib/theme-chalk/index.css'//element-ui的css
+Vue.use(ElementUI) //使用elementUI
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+import 'element-ui/lib/theme-chalk/index.css'//element-ui的css
+import axios from 'axios'
 Vue.config.productionTip = false
 
+// 本机地址
+ axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 挂在到Vue实例，后面可通过this调用
+Vue.prototype.$http = axios
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,49 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/Login'
+import Home from '@/components/Home'
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/login',
-      name: 'login',
-      component: Login
-    },
-    {
-      path: '/',
-      redirect: 'login'
-    }
-  ]
+
+// export default new Router({
+//   routes: [
+//     {
+//       path: '/login',
+//       name: 'login',
+//       component: Login
+//     },
+//     {
+//       path: '/',
+//       redirect: 'login'
+//     },
+//     {
+//       path: '/home',
+//       name: 'home',
+//       component: Home
+//     }
+//   ]
+// })
+
+const routes = [
+  { path: '/', redirect: '/login' },
+  { path: '/login', component: Login },
+  { path: '/home',
+    component: Home
+}
+
+]
+const router = new Router({
+  routes
 })
+// 挂载路由导航守卫,to表示将要访问的路径，from表示从哪里来，next是下一个要做的操作 next('/login')强制跳转login
+router.beforeEach((to, from, next) => {
+  // 访问登录页，放行
+  if (to.path === '/login') return next()
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  // 没有token, 强制跳转到登录页
+  if (!tokenStr) return next('/login')
+  next()
+})
+
+export default router
